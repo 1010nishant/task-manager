7e78719e365a3da87d5544631338e212f2f7d682
1010nishant <nishantjangid6377@gmail.com>
2025-03-23 22:40:01 +0530

    first commit

---
+++ src/newtab.tsx
@@ -0,0 +1,13 @@
+import MainNewTab from "~pages/MainNewTab";
+
+function IndexNewtab() {
+
+  return (
+    <div className="bg-red-400 flex flex-row">
+      <MainNewTab />
+      <MainNewTab />
+    </div>
+  )
+}
+
+export default IndexNewtab
--- src/popup.tsx
+++ src/popup.tsx
@@ -1,10 +1,14 @@
+import { Button } from "~components/ui/button";
 import "~main.css";
 
 
 function IndexPopup() {
   return (
     <div className=''>
-      <h1>Hello World</h1>
+      {/* <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        The Joke Tax Chronicles
+      </h1> */}
+      <Button variant="link">Destructive</Button>
     </div>
   )
 }
